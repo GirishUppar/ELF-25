57b7851b25b4d8fb46b0233303f151fa8fa7b90a
DELL <DELL@DESKTOP-27K4VSL>
2021-12-11 11:45:39 +0530

    Update

---
+++ TestYentra_Git_Hub/src/main/java/Test/TestYentra_Git_Hub/App.java
@@ -0,0 +1,13 @@
+package Test.TestYentra_Git_Hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestYentra_Git_Hub/src/test/java/Test/TestYentra_Git_Hub/AppTest.java
@@ -0,0 +1,20 @@
+package Test.TestYentra_Git_Hub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
